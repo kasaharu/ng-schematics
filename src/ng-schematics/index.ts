6df3c2a1c0576aa995329de97f37f7cebe4d8163
Wataru.Kasahara <kasahara1019@gmail.com>
2019-10-18 10:05:17 +0900

    README を参考にひな形を用意
      - ref. https://github.com/angular/angular-cli/blob/master/packages/angular_devkit/schematics/README.md#templating-1

--- src/ng-schematics/index.ts
+++ src/ng-schematics/index.ts
@@ -1,9 +1,40 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+// import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+//
+// // You don't have to export the function as default. You can also have more than one rule factory
+// // per file.
+// export function ngSchematics(_options: any): Rule {
+//   return (tree: Tree, _context: SchematicContext) => {
+//     return tree;
+//   };
+// }
 
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
-export function ngSchematics(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
+import { strings } from '@angular-devkit/core';
+import {
+  Rule,
+  SchematicContext,
+  SchematicsException,
+  Tree,
+  apply,
+  branchAndMerge,
+  mergeWith,
+  template,
+  url,
+} from '@angular-devkit/schematics';
+import { Schema as ClassOptions } from './schema';
+
+export default function(options: ClassOptions): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    if (!options.name) {
+      throw new SchematicsException('Option (name) is required.');
+    }
+
+    const templateSource = apply(url('./files'), [
+      template({
+        ...strings,
+        ...options,
+      }),
+    ]);
+
+    return branchAndMerge(mergeWith(templateSource));
   };
 }
