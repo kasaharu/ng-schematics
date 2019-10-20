f94d00dda16e3ba91825fc033919dfbb1d05bc6b
Wataru.Kasahara <kasahara1019@gmail.com>
2019-10-20 15:46:45 +0900

    生成されるファイルの中身を確認する spec を用意

--- src/adapter/index_spec.ts
+++ src/adapter/index_spec.ts
@@ -8,6 +8,31 @@ const fileName = 'hello-world';
 const generatedFileName = '/hello-world.adapter.ts';
 const generatedSpecFileName = '/hello-world.adapter.spec.ts';
 
+const generatedFile = `import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HelloWorldAdapter {
+  constructor() {}
+}`;
+
+const generatedSpecFile = `import { TestBed } from '@angular/core/testing';
+import { HelloWorldAdapter } from './hello-world.adapter';
+
+describe('HelloWorldAdapter', () => {
+  let adapter: HelloWorldAdapter;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    adapter = TestBed.inject(HelloWorldAdapter);
+  });
+
+  it('should be created', () => {
+    expect(adapter).toBeTruthy();
+  });
+});`;
+
 describe('adapter', () => {
   let runner: SchematicTestRunner;
   let tree: UnitTestTree;
@@ -22,5 +47,13 @@ describe('adapter', () => {
   it('生成されるファイル名の確認', () => {
     const outputFileList = [generatedSpecFileName, generatedFileName];
     expect(tree.files).toEqual(outputFileList);
+  });
+
+  it('生成される実装ファイルの内容確認', () => {
+    expect(tree.readContent(generatedFileName)).toBe(generatedFile);
+  });
+
+  it('生成されるテストファイルの内容確認', () => {
+    expect(tree.readContent(generatedSpecFileName)).toBe(generatedSpecFile);
   });
 });
