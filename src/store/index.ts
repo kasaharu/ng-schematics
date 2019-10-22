60fb04701caf41753e846295d599d8019dfb0692
Wataru.Kasahara <kasahara1019@gmail.com>
2019-10-22 13:13:50 +0900

    store の generate コードを実装

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,30 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { strings } from '@angular-devkit/core';
+import {
+  Rule,
+  SchematicContext,
+  SchematicsException,
+  Tree,
+  apply,
+  branchAndMerge,
+  mergeWith,
+  template,
+  url,
+} from '@angular-devkit/schematics';
+// import { Schema as ClassOptions } from './schema';
 
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
-export function generateStore(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
+export function generateStore(options: any): Rule {
+  return (_tree: Tree, _context: SchematicContext) => {
+    if (!options.name) {
+      throw new SchematicsException('Option (name) is required.');
+    }
+
+    const templateSource = apply(url('./files'), [
+      template({
+        ...strings,
+        ...options,
+      }),
+    ]);
+
+    return branchAndMerge(mergeWith(templateSource));
   };
 }
