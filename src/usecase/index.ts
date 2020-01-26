507c5ab51fe56af4b00e8fbf0451f6cb9a433f48
Wataru.Kasahara <kasahara1019@gmail.com>
2020-01-26 17:14:23 +0900

    [usecase] usecase が生成できるように実装

---
+++ src/usecase/index.ts
@@ -0,0 +1,63 @@
+import { experimental, normalize, strings } from '@angular-devkit/core';
+import { apply, applyTemplates, chain, mergeWith, move, Rule, SchematicsException, Tree, url } from '@angular-devkit/schematics';
+import { Schema as UsecaseSchema } from './schema';
+
+export function generate(options: UsecaseSchema): Rule {
+  return (tree: Tree) => {
+    // parse workspace string into JSON object
+    const workspace: experimental.workspace.WorkspaceSchema =
+      options.env === 'test' ? getWorkspaceForTestEnvironment() : getWorkspace(tree);
+
+    if (!options.project) {
+      options.project = workspace.defaultProject;
+    }
+
+    const projectName = options.project as string;
+    const project = workspace.projects[projectName];
+    const projectType = project.projectType === 'application' ? 'app' : 'lib';
+
+    if (options.path === undefined) {
+      options.path = `${project.sourceRoot}/${projectType}`;
+    }
+
+    // NOTE: 渡された name がパス付きになっていた場合パス部分を path として変換し最後の名前を name に残す
+    const [targetName, targetPath] = getAdjustNameAndPath(options.name, options.path);
+
+    const templateSource = apply(url('./files'), [applyTemplates({ ...strings, name: targetName }), move(normalize(targetPath as string))]);
+    return chain([mergeWith(templateSource)]);
+  };
+}
+
+function getWorkspace(tree: Tree): experimental.workspace.WorkspaceSchema {
+  const workspaceConfig = tree.read('/angular.json');
+  if (!workspaceConfig) {
+    throw new SchematicsException('Could not find Angular workspace configuration');
+  }
+
+  // convert workspace to string
+  const workspaceContent = workspaceConfig.toString();
+
+  // parse workspace string into JSON object
+  return JSON.parse(workspaceContent);
+}
+
+function getWorkspaceForTestEnvironment(): experimental.workspace.WorkspaceSchema {
+  return {
+    projects: {
+      'ng-schematics': { projectType: 'application', sourceRoot: 'src', root: '', prefix: 'app' },
+    },
+    defaultProject: 'ng-schematics',
+    version: 1,
+  };
+}
+
+function getAdjustNameAndPath(originalName: string, originalPath: string): string[] {
+  const splitedName = originalName.split('/');
+  const hasPathInName = splitedName.length >= 2;
+  const lastIndex = splitedName.length - 1;
+
+  const adjustName = !hasPathInName ? originalName : splitedName[lastIndex];
+  const adjustPath = !hasPathInName ? originalPath : `${originalPath}/${splitedName.filter((_, index) => index !== lastIndex).join('/')}`;
+
+  return [adjustName, adjustPath];
+}
